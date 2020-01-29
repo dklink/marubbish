e66ca70c22d48bb4c441afd0a3afc98631eb257d
Doug Klink <dougdklink3@gmail.com>
2020-01-29 17:15:42 +1300

    added chlorophyll z dependence, tested

---
+++ src/matlab_fouling/chlorophyll/get_chl_at_z.m
@@ -0,0 +1,15 @@
+function chl_z = get_chl_at_z(z, chl_surf, z_eu)
+%get_chl_at_z Simplified vertical profile, taken from Uitz 2006 mixed profile
+%   z: depth (m)
+%   chl_surf: concentration of chl_a at surface (mg m^-3)
+%   z_eu: euphotic depth (m)
+%   return: chlorophyll-a concentration at depth z (mg m^-3)
+
+    if z < z_eu
+        chl_z = chl_surf;
+    elseif z < 2*z_eu
+        chl_z = chl_surf - chl_surf/z_eu * (z-z_eu);  % linear decrease, chl_surf to 0, from z_eu to 2z_eu
+    else
+        chl_z = 0;
+    end
+end
