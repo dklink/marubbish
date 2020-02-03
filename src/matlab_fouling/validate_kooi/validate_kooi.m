579213e00ef713ccb022e52f45291fe8d794d1c6
Doug Klink <dougdklink3@gmail.com>
2020-02-04 12:37:20 +1300

    set up fig 1 reproduction, somewhat successful

--- src/matlab_fouling/validate_kooi/validate_kooi.m
+++ src/matlab_fouling/validate_kooi/validate_kooi.m
@@ -1,86 +1,110 @@
-%kooi fig 1
-seconds_per_hour = 3600;
-seconds_per_day = seconds_per_hour*24;
-hours_per_week = 24*7;
+%fig1a();
+%fig1b();
+%fig1c();
+%fig1d();
 
-NP_lat = 25.428321;   % lat and lon of spot near hawaii
-NP_lon = -151.773256; %
+function fig1a()
+    %kooi fig 1a
+    num_days = 110;
+    dt_hours = .1;  % behavior stabilizes below .2 or so (.7 mimics kooi exactly)
+    particle_radius = 1e-3; % m
+    [t, z] = get_t_vs_z(num_days, dt_hours, particle_radius);
 
-particle_radius = .001;
-p = Particle(particle_radius, kooi_constants.rho_LDPE, 0, NP_lat, NP_lon, 0);
-
-dt_hours = .05;  % this needs to be like .1 * period of oscillation for good accuracy
-dt = seconds_per_hour*dt_hours;
-num_days = 110;
-t = 1:dt:num_days*seconds_per_day;
-A = zeros(1, length(t));
-z = zeros(1, length(t));
-rho = zeros(1, length(t));
-
-chl_surf_np = kooi_constants.chl_surf_np;
-chl_ave_np = kooi_constants.chl_ave_np;
-z(1) = p.z;
-A(1) = p.A;
-rho(1) = p.rho_tot;
-V_s = 0;
-for i=2:length(t)
-    I_surf = I_vs_time(t(i));
-    T_z = T_vs_z(p.z);
-    I_z = get_light_at_z(p.z, I_surf, chl_ave);
-    S_z = S_vs_z(p.z);
-    dAdt = get_algae_flux_for_particle(p, S_z, T_z, chl_surf, I_z);
-    
-    p.A = p.A + dAdt * dt;
-    
-    % not sure if this properly approximates the physics
-    V_s = get_settling_velocity(p, S_z, T_z);
-    
-    p.z = p.z + V_s * dt;
-    if p.z < 0  % silly particle, you can't fly
-        p.z = 0;
-    end
-    rho(i) = p.rho_tot;
-    z(i) = p.z;
-    A(i) = p.A;
+    z = z(t/seconds_per_day > 100);
+    t = t(t/seconds_per_day > 100);
+    plot_t_vs_z(t, z, '1 mm');
 end
 
-% plot just days 100-110
+function fig1b()
+    %kooi fig 1b
+    num_days = 150;
+    dt_hours = 1;  % behavior stabilizes below 1 or so
+    particle_radius = 1e-4; % m
+    [t, z] = get_t_vs_z(num_days, dt_hours, particle_radius);
 
-A = A(t/seconds_per_day > 100);
-z = z(t/seconds_per_day > 100);
-rho = rho(t/seconds_per_day > 100);
-t = t(t/seconds_per_day > 100);
+    z = z(t/seconds_per_day > 100);
+    t = t(t/seconds_per_day > 100);
+    plot_t_vs_z(t, z, '0.1 mm');
+end
 
+function fig1c()
+    %kooi fig 1c
+    num_days = 1000;
+    dt_hours = 1;  % behavior stabilizes below 2 or so
+    particle_radius = 1e-5; % m
+    [t, z] = get_t_vs_z(num_days, dt_hours, particle_radius);
 
-figure;
-set(0, 'DefaultLineLineWidth', 2);
-plot(t/seconds_per_day, z);
-xlabel('days');
-ylabel('depth (m)');
-set(gca, 'YDir','reverse');
-title(sprintf('radius = %.2fmm', particle_radius*1000));
-ylim([0, 80]);
+    z = z(t/seconds_per_day > 100);
+    t = t(t/seconds_per_day > 100);
+    plot_t_vs_z(t, z, '10 \mum');
+end
 
-%{
-figure;
-plot(t/seconds_per_day, A);
-xlabel('days');
-ylabel('algae count');
-title(sprintf('radius = %.2fmm', particle_radius*1000));
-%}
+function fig1d()
+    %kooi fig 1b
+    num_days = 10000;
+    dt_hours = 4;  % behavior stabilizes below 5 or so
+    particle_radius = 1e-6; % m
+    [t, z] = get_t_vs_z(num_days, dt_hours, particle_radius);
 
-figure;
-plot(t/seconds_per_day, rho);
-xlabel('days');
-ylabel('density (kg m^{-3})');
-title(sprintf('radius = %.2fmm', particle_radius*1000));
-%}
+    z = z(t/seconds_per_day > 100);
+    t = t(t/seconds_per_day > 100);
+    plot_t_vs_z(t, z, '1 \mum');
+end
 
-% was trying to get an ode to work since kooi says she did this
-%{
-tspan = 0:dt:1000*seconds_per_day;
+function plot_t_vs_z(t, z, plot_title)
+    figure;
+    set(0, 'DefaultLineLineWidth', 2);
+    plot(t/seconds_per_day, z);
+    xlabel('days');
+    ylabel('depth (m)');
+    set(gca, 'YDir','reverse');
+    title(plot_title);
+    ylim([0, 80]);
+end
 
-z0 = 0;
-[t,z] = ode23(@(t,z) odefcn(t, z, p, dt), tspan, z0);
-plot(t, z);
-%}
+function [t, z] = get_t_vs_z(num_days, dt_hours, particle_radius)
+    % gets z vs t for a LDPE particle in the North Pacific
+        % (for replicating kooi fig 1)
+        % particle_radius in meters
+    % returns: [t, z] ([seconds, meters])
+
+    NP_lat = 25.428321;   % lat and lon of spot near hawaii
+    NP_lon = -151.773256; %
+
+    p = Particle(particle_radius, kooi_constants.rho_LDPE, 0, NP_lat, NP_lon, 0);
+
+    dt = seconds_per_hour*dt_hours;
+    t = 1:dt:num_days*seconds_per_day;
+    z = zeros(1, length(t));
+
+    chl_surf_np = kooi_constants.chl_surf_np;
+    chl_ave_np = kooi_constants.chl_ave_np;
+    z(1) = p.z;
+    for i=2:length(t)
+        I_surf = I_vs_time(t(i));
+        T_z = T_vs_z(p.z);
+        I_z = get_light_at_z(p.z, I_surf, chl_ave_np);
+        S_z = S_vs_z(p.z);
+        dAdt = get_algae_flux_for_particle(p, S_z, T_z, chl_surf_np, I_z);
+
+        p.A = p.A + dAdt * dt;
+
+        % this approximates the position function
+        V_s = get_settling_velocity(p, S_z, T_z);
+        new_z = p.z + V_s * dt;
+        if new_z < 0  % silly particle, you can't fly
+            new_z = 0;
+        end
+        
+        p.z = new_z;
+        z(i) = new_z;
+    end
+end
+
+function sph = seconds_per_hour()
+    sph = 3600;
+end
+
+function spd = seconds_per_day()
+    spd = seconds_per_hour*24;
+end
